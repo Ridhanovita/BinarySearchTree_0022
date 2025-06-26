05be72352c771ed411b8d3ae0878f89b6871d3df
Ridhanovita <novitaridha796@gmail.com>
2025-06-26 12:12:19 +0700

    switch case menu

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -158,5 +158,30 @@ int main()
         char ch;
         cin >> ch;
         cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            x.insert();
+            break;
+        }
+        case '2':
+        {
+            x.inorder(x.ROOT);
+            break;
+        }
+        case '3':
+        {
+
+            x.preorder(x.ROOT);
+            break;
+        }
+        case '4':
+        {
+
+            x.postorder(x.ROOT);
+            break;
+        }
     }
 }
