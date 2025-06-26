a9d473704d9eb71aba39ec31beee944c169d2757
Ridhanovita <novitaridha796@gmail.com>
2025-06-26 12:05:57 +0700

    fungsi inorder

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -89,4 +89,19 @@ public:
                 currentNode = currentNode->rightchild;
         }
     }
+
+    void inorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+
+        inorder(ptr->leftchild);
+        cout << ptr->info<< " "; // parent
+        inorder(ptr->rightchild);
+    }
 };
