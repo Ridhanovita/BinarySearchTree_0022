5d64522be832a3263eb9d198bdf4bfd819ebd894
Ridhanovita <novitaridha796@gmail.com>
2025-06-26 11:54:56 +0700

    definisi class node dan konstruktor

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+class Node
+{
+public:
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    // Constructor for the class node
+    Node()
+    {
+        leftchild = nullptr;  // Initializing left chils to null
+        rightchild = nullptr; // Initializing right child to null
+    }
+};
