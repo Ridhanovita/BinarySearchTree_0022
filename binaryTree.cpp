a3b3aea2c1903eaad50a70b4e70cee993c20c77c
Ridhanovita <novitaridha796@gmail.com>
2025-06-26 12:04:40 +0700

    fungsi search node

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -74,4 +74,19 @@ public:
             return;
         }
     }
+
+    void Search (int element, Node *&parent, Node *&currentNode)
+    {
+        // This function seraches the currentNode of the specified Node as well as the current Node of its parent
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else 
+                currentNode = currentNode->rightchild;
+        }
+    }
 };
