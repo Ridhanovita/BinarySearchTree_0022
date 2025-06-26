cf7e9341f1ba8c808c7368ed4c691ec541644471
Ridhanovita <novitaridha796@gmail.com>
2025-06-26 12:09:22 +0700

    fungsi isEmpty untuk mnegecek apakah tree kosong

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -134,4 +134,10 @@ public:
         postorder(ptr->rightchild);
         cout << ptr->info << " ";
     }
+
+    bool isEmpty()
+    {
+        // Checks if the tree is empty
+        return ROOT == nullptr;
+    }
 };
